1d75f3515e7bdb8f43d0c6607411f14711382361
Behrad Niazmand <bniazmand@pralinee.pld.ttu.ee>
2017-11-15 13:57:53 +0200

    Synthesis scripts uploaded

---
+++ Synthesis_scripts/set_run.tcl
@@ -0,0 +1,28 @@
+# set IMPLEMENTATION_NAME "pparch"
+# set MODULE_INSTANCE_NAME "U1"
+
+
+compile_ultra -no_seq_output_inversion -no_autoungroup
+# compile -exact_map
+
+# Use this if you want to remove unconnected ports from the design during optimization
+# remove_unconnected_ports -blast_buses [find -hierarchy cell {"*"}]
+# compile -incr
+
+# check_design
+
+# Use to change implementation of a specific module (for example adder or multiplier from DesignWare Library)
+# set_implementation $IMPLEMENTATION_NAME $MODULE_INSTANCE_NAME
+# compile -incr
+
+# Shows implementation details for the module(s)
+report_resources
+
+# Use to break the hierarchy and make the design flat (only including gates)
+# ungroup -flatten -all
+# compile -exact_map -ungroup_all
+
+# ungroup -flatten -all
+# compile_ultra -no_seq_output_inversion -no_autoungroup
+
+check_design
