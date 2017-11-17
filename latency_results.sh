22c150c04879dc85ac5e544b99e85fd4a0a3061c
siavooshpayandehazad <siavooshpayandeh@gmail.com>
2017-11-17 12:10:31 +0200

    bug fix in folder name

--- latency_results.sh
+++ latency_results.sh
@@ -1,18 +1,18 @@
 
-touch Synthesis_Results/critical_sent.txt
-touch Synthesis_Results/critical_received.txt
+touch RTL_baseline/critical_sent.txt
+touch RTL_baseline/critical_received.txt
 
 
-grep 'From 15' Synthesis_Results/sent.txt > Synthesis_Results/critical_sent.txt
-grep 'From 0'  Synthesis_Results/sent.txt >> Synthesis_Results/critical_sent.txt
+grep 'From 15' RTL_baseline/sent.txt > RTL_baseline/critical_sent.txt
+grep 'From 0'  RTL_baseline/sent.txt >> RTL_baseline/critical_sent.txt
 
-grep 'From: 15'  Synthesis_Results/received.txt > Synthesis_Results/critical_received.txt
-grep 'From: 0'  Synthesis_Results/received.txt >> Synthesis_Results/critical_received.txt
+grep 'From: 15'  RTL_baseline/received.txt > RTL_baseline/critical_received.txt
+grep 'From: 0'  RTL_baseline/received.txt >> RTL_baseline/critical_received.txt
 
 echo "OVERALL SYSTEM QoS:"
 echo ""
-python calculate_latency.py -S Synthesis_Results/sent.txt -R Synthesis_Results/received.txt
+python calculate_latency.py -S RTL_baseline/sent.txt -R RTL_baseline/received.txt
 echo "--------------------------------------------"
 echo "CRITICAL REGION QoS:"
 echo ""
-python calculate_latency.py -S Synthesis_Results/critical_sent.txt -R Synthesis_Results/critical_received.txt
+python calculate_latency.py -S RTL_baseline/critical_sent.txt -R RTL_baseline/critical_received.txt
